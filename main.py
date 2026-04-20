8a7eb7486d962b6f6cb0e7ff44bc662a30249635
tans-01 <225133498+tans-01@users.noreply.github.com>
2026-04-21 03:36:03 +1000

    implementing the huffman code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import heapq
+
 def compressor(file_path):
     frequency = {}
 
@@ -9,8 +11,18 @@ def compressor(file_path):
             frequency[char] += 1
         else:            
             frequency[char] = 1
-    frequency_sorted = sorted(frequency.items(), key = lambda x: x[1], reverse=True)
-    return frequency_sorted
+    frequency_sorted = sorted((char, freq) for char, freq in frequency.items())
+    heap = frequency_sorted
+    heapq.heapify(heap)
+    return heap
+def huffman_encoding(heap):
+    while len(heap) > 1:
+        left = heapq.heappop(heap)
+        right = heapq.heappop(heap)
+        merged = (left[0] + right[0], left[1] + right[1], left, right)
+        heapq.heappush(heap, merged)
+
+    return heapq.heappop(heap)
 
 def main():
     file_path = "input.txt"
@@ -18,5 +30,10 @@ def main():
 
     for char, freq in frequency:
         print(f"Character: '{char}' Frequency: {freq}")
+    root = huffman_encoding(frequency)
+    print(f"\nTree root frequency: {root[1]}")
+    with open(file_path, "r") as file:
+        content = file.read()
+    print(len(content))
 
 main()
