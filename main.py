123f3b1e76aac4df88797acb38cdc03947235177
tans-01 <225133498+tans-01@users.noreply.github.com>
2026-04-21 04:54:58 +1000

    generating huffman codings from the tree

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import heapq
 
+
+class Node:
+    def __init__(self, char, freq):
+     self.char = char
+     self.freq = freq
+     self.left = None
+     self.right = None
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
 def compressor(file_path):
     frequency = {}
 
@@ -11,29 +22,45 @@ def compressor(file_path):
             frequency[char] += 1
         else:            
             frequency[char] = 1
-    frequency_sorted = sorted((char, freq) for char, freq in frequency.items())
+    frequency_sorted = [Node(char, freq) for char, freq in frequency.items()]
     heap = frequency_sorted
     heapq.heapify(heap)
     return heap
+
+
 def huffman_encoding(heap):
     while len(heap) > 1:
         left = heapq.heappop(heap)
         right = heapq.heappop(heap)
-        merged = (left[0] + right[0], left[1] + right[1], left, right)
+        merged = Node(None, left.freq + right.freq)
+        merged.left = left
+        merged.right = right
         heapq.heappush(heap, merged)
 
     return heapq.heappop(heap)
 
+def generate_code(node, code, code_dict={}):
+    if node is None:
+        return
+    if node.right==None and node.left==None:
+        code_dict[node.char] = code
+    if node:
+        generate_code(node.left, code + "0", code_dict)
+        generate_code(node.right, code + "1", code_dict)
+    return code_dict
 def main():
     file_path = "input.txt"
     frequency = compressor(file_path)
 
-    for char, freq in frequency:
-        print(f"Character: '{char}' Frequency: {freq}")
+    for node in frequency:
+        print(f"Character: '{node.char}' Frequency: {node.freq}")
     root = huffman_encoding(frequency)
-    print(f"\nTree root frequency: {root[1]}")
+    print(f"\nTree root frequency: {root.freq}")
     with open(file_path, "r") as file:
         content = file.read()
     print(len(content))
+    codes = generate_code(root, "")
 
+    for char, code in codes.items():
+     print(f"'{char}' : {code}")
 main()
