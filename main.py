338f061d64758f2acba03092446d3c6d354b817f
tans-01 <225133498+tans-01@users.noreply.github.com>
2026-04-21 02:29:53 +1000

    sorted the frequency

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def compressor(file_path):
             frequency[char] += 1
         else:            
             frequency[char] = 1
-
-    return frequency
+    frequency_sorted = sorted(frequency.items(), key = lambda x: x[1], reverse=True)
+    return frequency_sorted
 
 def main():
     file_path = "input.txt"
     frequency = compressor(file_path)
 
-    for char, freq in frequency.items():
+    for char, freq in frequency:
         print(f"Character: '{char}' Frequency: {freq}")
 
 main()
